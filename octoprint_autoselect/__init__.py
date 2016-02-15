fe3f0b8aed9dc8dbbb14e8564e114d4c2a1f6c60
scampa123 <scampa123@yahoo.com>
2016-02-14 19:32:42 -0500

    Update __init__.py
    Updated for fork/name change and added code to print the file.

--- octoprint_autoselect/__init__.py
+++ octoprint_autoselect/__init__.py
@@ -10,7 +10,7 @@ class AutoselectPlugin(octoprint.plugin.EventHandlerPlugin):
 			return
 
 		if not self._printer.is_ready():
-			self._logger.debug("Printer is not ready, not autoselecting uploaded file")
+			self._logger.debug("Printer is not ready, not autoprinting uploaded file")
 			return
 
 		filename = payload["file"]
@@ -23,11 +23,12 @@ class AutoselectPlugin(octoprint.plugin.EventHandlerPlugin):
 			path = self._file_manager.path_on_disk(target, filename)
 			sd = False
 
-		self._logger.info("Selecting {} on {} that was just uploaded".format(filename, target))
+		self._logger.info("Selecting and printing {} on {} that was just uploaded".format(filename, target))
 		self._printer.select_file(path, sd, False)
+		self._printer.start_print()
 
-__plugin_name__ = "Autoselect Plugin"
+__plugin_name__ = "AutoPrint Plugin"
 
 def __plugin_load__():
 	global __plugin_implementation__
-	__plugin_implementation__ = AutoselectPlugin()
+	__plugin_implementation__ = AutoPrintPlugin()
